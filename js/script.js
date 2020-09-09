7807ca36d4a2eef747ae048622f63e95d8d99cf2
veedahv <veedahv55@gmail.com>
2020-09-09 12:48:47 +0100

    new commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+    let hamburger = document.getElementsByClassName("hamburger")[0];
+
+function hamburg() {
+    let links = document.getElementsByClassName("nav-a")[0];
+    links.classList.toggle("toggle");
+}
